3ade9ead2e949a3f4df0e67dc2ee9704f7050340
allexradu <eusceptic@gmail.com>
2020-04-18 20:31:24 +0300

    Version 1.0.1.2

--- Google_Bot/__init__.py
+++ Google_Bot/__init__.py
@@ -71,7 +71,7 @@ class GoogleBot:
 
 
 print('========================================================================')
-print('|           Google_Bot 1.0.1.1 by Allex Radu [www.ATFR.net]             |')
+print('|           Google_Bot 1.0.1.2 by Allex Radu [www.ATFR.net]             |')
 print('|     Get the latest version at https://github.com/allexradu/gBot       |')
 print('========================================================================')
 print('| Instructions: Save your Excel Workbook as "a.xls" and place it in     |')
---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+import Google_Bot
+
+setup(
+    name = 'Google_Bot',
+    version = '1.0.1.1',
+
+    py_modules = [Google_Bot],
+    url = 'https://github.com/allexradu/Google_Bot',
+    license = 'MIT License',
+    author = 'Allex Radu',
+    author_email = 'dev@allexradu.com',
+    description = 'This bot allows you to quickly download product images from your online shop using Google Image '
+                  'Search. '
+)
